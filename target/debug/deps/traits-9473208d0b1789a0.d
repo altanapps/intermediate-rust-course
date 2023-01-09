8ddb73c4d4718764f942f20da1a2942df75a1006
altanapps <futureisaltan@gmail.com>
2023-01-09 08:14:34 +0000

    exercise for traits finished

---
+++ target/debug/deps/traits-9473208d0b1789a0.d
@@ -0,0 +1,5 @@
+/Users/altantutar/Desktop/github/intermediate-rust-course/target/debug/deps/traits-9473208d0b1789a0: exercise/traits/src/main.rs
+
+/Users/altantutar/Desktop/github/intermediate-rust-course/target/debug/deps/traits-9473208d0b1789a0.d: exercise/traits/src/main.rs
+
+exercise/traits/src/main.rs:
---
+++ target/debug/traits.d
@@ -0,0 +1,1 @@
+/Users/altantutar/Desktop/github/intermediate-rust-course/target/debug/traits: /Users/altantutar/Desktop/github/intermediate-rust-course/exercise/traits/src/main.rs
